73ec0ce210d5f9f702fa54254c9d627dbe77c76b
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2019-12-29 07:49:32 -0800

    Use vector.

--- library/Library.hs
+++ library/Library.hs
@@ -2,25 +2,41 @@
 
 module Library where
 
-data Type = Or [Type] | And [Type] deriving Show
+import qualified Data.Foldable as F
+import qualified Data.Set as S
+import qualified Data.Vector as V
+
+data Type = Or (V.Vector Type) | And (V.Vector Type) deriving Show
 
 calculateSize :: Type -> Integer
 calculateSize (Or items) = sum $ fmap calculateSize items
 calculateSize (And items) = product $ fmap calculateSize items
 
-enumerate :: Type -> [Integer]
-enumerate (Or []) = []
-enumerate (And []) = [0]
-enumerate (Or (x : xs)) = current ++ fmap (+ currentSize) rest
-  where
-  rest = enumerate (Or xs)
-  currentSize = fromIntegral $ length current
-  current = enumerate x
-enumerate (And (x : xs)) =
-  concatMap (\c -> fmap (\r -> r + c * restSize) rest) current
-  where
-  restSize = fromIntegral $ length rest
-  rest = enumerate (And xs)
-  current = enumerate x
+enumerate :: Type -> V.Vector Integer
+enumerate (Or items) =
+  F.foldl'
+    (\results next -> results <> fmap (+ fromIntegral (V.length results)) next)
+    V.empty
+    (fmap enumerate items)
+
+enumerate (And items) =
+  F.foldl'
+    (\results next -> F.foldMap (\n -> fmap (\r -> n + r * (fromIntegral (V.length next))) results) next)
+    (V.singleton 0)
+    (fmap enumerate items)
+
+-- enumerate (Or []) = []
+-- enumerate (And []) = [0]
+-- enumerate (Or (x : xs)) = current ++ fmap (+ currentSize) rest
+--   where
+--   rest = enumerate (Or xs)
+--   currentSize = fromIntegral $ length current
+--   current = enumerate x
+-- enumerate (And (x : xs)) =
+--   concatMap (\c -> fmap (\r -> r + c * restSize) rest) current
+--   where
+--   restSize = fromIntegral $ length rest
+--   rest = enumerate (And xs)
+--   current = enumerate x
 
 main = putStrLn "Hi"
--- test/Test.hs
+++ test/Test.hs
@@ -1,36 +1,50 @@
 import Test.Hspec
 import Test.QuickCheck
 import Library hiding (main)
+import qualified Data.Vector as V
+import qualified Data.Set as S
+import Test.QuickCheck.Instances.Vector ()
+
+mkOr :: [Type] -> Type
+mkOr = Or . V.fromList
+
+mkAnd :: [Type] -> Type
+mkAnd = And . V.fromList
 
 main = hspec $ do
   let
-    v0 = Or []
-    v1 = And []
-    v2 = Or [v1, v1]
-    v3 = Or [v1, v1, v1]
+    v0 = mkOr []
+    v1 = mkAnd []
+    v2 = mkOr [v1, v1]
+    v3 = mkOr [v1, v1, v1]
 
   describe "calculateSize" $ do
-    it "empty Or" $ calculateSize (Or []) `shouldBe` 0
-    it "empty And" $ calculateSize (And []) `shouldBe` 1
-    it "1 or 1" $ calculateSize (Or [v1, v1]) `shouldBe` 2
-    it "or [1,1,1] = 3" $ calculateSize (Or [v1, v1, v1]) `shouldBe` 3
-    it "or [1,2] = 3" $ calculateSize (Or [v1, v2]) `shouldBe` 3
-    it "and [1,2] = 2" $ calculateSize (And [v1, v2]) `shouldBe` 2
-    it "and [2,2] = 4" $ calculateSize (And [v2, v2]) `shouldBe` 4
-    it "or [2,3] = 5" $ calculateSize (Or [v2, v3]) `shouldBe` 5
-    it "and [2,3] = 6" $ calculateSize (And [v2, v3]) `shouldBe` 6
+    it "empty or" $ calculateSize (mkOr []) `shouldBe` 0
+    it "empty and" $ calculateSize (mkAnd []) `shouldBe` 1
+    it "1 or 1" $ calculateSize (mkOr [v1, v1]) `shouldBe` 2
+    it "or [1,1,1] = 3" $ calculateSize (mkOr [v1, v1, v1]) `shouldBe` 3
+    it "or [1,2] = 3" $ calculateSize (mkOr [v1, v2]) `shouldBe` 3
+    it "and [1,2] = 2" $ calculateSize (mkAnd [v1, v2]) `shouldBe` 2
+    it "and [2,2] = 4" $ calculateSize (mkAnd [v2, v2]) `shouldBe` 4
+    it "or [2,3] = 5" $ calculateSize (mkOr [v2, v3]) `shouldBe` 5
+    it "and [2,3] = 6" $ calculateSize (mkAnd [v2, v3]) `shouldBe` 6
+
+  let enumerateSet = S.fromList . V.toList . enumerate
 
   describe "enumerate" $ do
-    it "empty Or" $ enumerate (Or []) `shouldBe` []
-    it "empty And" $ enumerate (And []) `shouldBe` [0]
-    it "or [1,1]" $ enumerate (Or [v1,v1]) `shouldBe` [0,1]
-    it "or [1,1,1]" $ enumerate (Or [v1,v1,v1]) `shouldBe` [0,1,2]
-    it "or [1,2]" $ enumerate (Or [v1,v2]) `shouldBe` [0,1,2]
-    it "and [2,3] = 6" $ enumerate (And [v2, v3]) `shouldBe` [0..5]
-    it "and [6,3] = 18" $ enumerate (And [And [v2, v3], v3]) `shouldBe` [0..17]
+    it "empty or" $ enumerateSet (mkOr []) `shouldBe` S.fromList []
+    it "empty and" $ enumerateSet (mkAnd []) `shouldBe` S.fromList [0]
+    it "or [1,1]" $ enumerateSet (mkOr [v1,v1]) `shouldBe` S.fromList [0,1]
+    it "or [1,1,1]" $ enumerateSet (mkOr [v1,v1,v1]) `shouldBe` S.fromList [0,1,2]
+    it "or [1,2]" $ enumerateSet (mkOr [v1,v2]) `shouldBe` S.fromList [0,1,2]
+    it "and [2,3] = 6" $ enumerateSet (mkAnd [v2, v3]) `shouldBe` S.fromList [0..5]
+    it "and [6,3] = 18" $ enumerateSet (mkAnd [mkAnd [v2, v3], v3]) `shouldBe` S.fromList [0..17]
 
   describe "calculateSize and enumerate" $ do
+    -- it "equal sizes" $ do
+    --   labelledExamples $ withMaxSuccess 4 $ property $ \x -> collect x $ fromIntegral (calculateSize x) == length (enumerate x)
+    --   True `shouldBe` False
     it "equal sizes" $ withMaxSuccess 3 $ property $ \x -> fromIntegral (calculateSize x) == length (enumerate x)
 
 instance Arbitrary Type where
-  arbitrary = oneof [fmap Or arbitrary, fmap And arbitrary]
+  arbitrary = oneof [fmap mkOr arbitrary, fmap And arbitrary]
