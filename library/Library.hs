a43dadfe7e8428378d39a8a96b0d212c01b82445
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2019-12-29 06:04:10 -0800

    Implement enumerate.

--- library/Library.hs
+++ library/Library.hs
@@ -8,8 +8,19 @@ calculateSize :: Type -> Integer
 calculateSize (Or items) = sum $ fmap calculateSize items
 calculateSize (And items) = product $ fmap calculateSize items
 
--- enumerate :: Type -> [Integer]
--- enumerate (Or []) = []
--- enumerate (And []) = [0]
+enumerate :: Type -> [Integer]
+enumerate (Or []) = []
+enumerate (And []) = [0]
+enumerate (Or (x : xs)) = current ++ fmap (+ currentSize) rest
+  where
+  rest = enumerate (Or xs)
+  currentSize = fromIntegral $ length current
+  current = enumerate x
+enumerate (And (x : xs)) =
+  concatMap (\c -> fmap (\r -> r + c * restSize) rest) current
+  where
+  restSize = fromIntegral $ length rest
+  rest = enumerate (And xs)
+  current = enumerate x
 
 main = putStrLn "Hi"
--- test/Test.hs
+++ test/Test.hs
@@ -2,19 +2,27 @@ import Test.Hspec
 import Library hiding (main)
 
 main = hspec $ do
-  it "empty Or" $ calculateSize (Or []) `shouldBe` 0
-  it "empty And" $ calculateSize (And []) `shouldBe` 1
-
   let
     v0 = Or []
     v1 = And []
     v2 = Or [v1, v1]
     v3 = Or [v1, v1, v1]
 
-  it "1 or 1" $ calculateSize (Or [v1, v1]) `shouldBe` 2
-  it "or [1,1,1] = 3" $ calculateSize (Or [v1, v1, v1]) `shouldBe` 3
-  it "or [1,2] = 3" $ calculateSize (Or [v1, v2]) `shouldBe` 3
-  it "and [1,2] = 2" $ calculateSize (And [v1, v2]) `shouldBe` 2
-  it "and [2,2] = 4" $ calculateSize (And [v2, v2]) `shouldBe` 4
-  it "or [2,3] = 5" $ calculateSize (Or [v2, v3]) `shouldBe` 5
-  it "and [2,3] = 6" $ calculateSize (And [v2, v3]) `shouldBe` 6
+  describe "calculateSize" $ do
+    it "empty Or" $ calculateSize (Or []) `shouldBe` 0
+    it "empty And" $ calculateSize (And []) `shouldBe` 1
+    it "1 or 1" $ calculateSize (Or [v1, v1]) `shouldBe` 2
+    it "or [1,1,1] = 3" $ calculateSize (Or [v1, v1, v1]) `shouldBe` 3
+    it "or [1,2] = 3" $ calculateSize (Or [v1, v2]) `shouldBe` 3
+    it "and [1,2] = 2" $ calculateSize (And [v1, v2]) `shouldBe` 2
+    it "and [2,2] = 4" $ calculateSize (And [v2, v2]) `shouldBe` 4
+    it "or [2,3] = 5" $ calculateSize (Or [v2, v3]) `shouldBe` 5
+    it "and [2,3] = 6" $ calculateSize (And [v2, v3]) `shouldBe` 6
+
+  describe "enumerate" $ do
+    it "empty Or" $ enumerate (Or []) `shouldBe` []
+    it "empty And" $ enumerate (And []) `shouldBe` [0]
+    it "or [1,1]" $ enumerate (Or [v1,v1]) `shouldBe` [0,1]
+    it "or [1,1,1]" $ enumerate (Or [v1,v1,v1]) `shouldBe` [0,1,2]
+    it "or [1,2]" $ enumerate (Or [v1,v2]) `shouldBe` [0,1,2]
+    it "and [2,3] = 6" $ enumerate (And [v2, v3]) `shouldBe` [0..5]
