56bf38b55f66de8523053607be0f6ce066792483
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2020-02-27 14:39:05 -0800

    Add some comments.

---
+++ README.md
@@ -0,0 +1,75 @@
+# לספור
+Getting back to basics: learning how to count.
+
+## Sizes of possible distributions
+
+```haskell
+distributionSizeSequence n
+```
+
+Given a set of cardinality `n`, count the number of possible distributions for a lengths `[0..]`.
+
+Example: Bits
+
+A `bit` has cardinality 2.
+
+0. If you have 0 bits, you vacuously have one way to distribute them (which is distributing nothing)
+1. If you have 1 bit, you can either have a zero bit or a one bit, so two ways total.
+2. If you have 2 bits, you can have 3 possible distributions:
+  * 0 zeros and 2 ones
+  * 1 zero and 1 one
+  * 2 zeros and 0 ones
+3. If you have 3 bits, you can have 4 possible distributions:
+  * 0 zeros and 3 ones
+  * 1 zero and 2 ones
+  * 2 zeros and 1 one
+  * 3 zeros and 0 ones
+
+Things get more interesting for sets of cardinality 3 or more.
+
+These are apparently the same as the [binomial coefficients](https://en.wikipedia.org/wiki/Binomial_coefficient).
+
+More precisely:
+```haskell
+distributionSizeSequence k == fmap (\n -> choose n (k - 1)) [(k - 1)..]
+
+checkProp c k = take c (fmap fromIntegral (distributionSizeSequence k)) == take c (fmap (\n -> choose n (k - 1)) [(k - 1)..])
+and $ fmap (checkProp 7) [2..8]
+```
+
+### distributionSizeSequence 2
+```haskell
+[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67
+```
+
+### distributionSizeSequence 3
+```haskell
+[1,3,6,10,15,21,28,36,45,55,66,78,91,105,120,136,153,171,190,210,231,253,276,300,325,351,378,406,435,465,496,528,561,595,630,666,703,741,780,820,861,903,946,990,1035,1081,1128,1176,1225,1275,1326,1378
+```
+[A000217](https://oeis.org/A000217)  Triangular numbers: a(n) = binomial(n+1,2) = n(n+1)/2 = 0 + 1 + 2 + ... + n.
+[A161680](https://oeis.org/A161680)  a(n) = binomial(n,2): number of size-2 subsets of {0,1,...,n} that contain no consecutive integers.
+[A105340](https://oeis.org/A105340)  a(n) = n*(n+1)/2 mod 2048.
+
+### distributionSizeSequence 4
+```haskell
+[1,4,10,20,35,56,84,120,165,220,286,364,455,560,680,816,969,1140,1330,1540,1771,2024,2300,2600,2925,3276,3654,4060,4495,4960,5456
+```
+[A000292](https://oeis.org/A000292)  Tetrahedral (or triangular pyramidal) numbers: a(n) = C(n+2,3) = n*(n+1)*(n+2)/6.
+
+### distributionSizeSequence 5
+```haskell
+[1,5,15,35,70,126,210,330,495,715,1001,1365,1820,2380,3060,3876,4845,5985,7315,8855
+```
+[A000332](https://oeis.org/A000332)  Binomial coefficient binomial(n,4) = n*(n-1)*(n-2)*(n-3)/24.
+
+### distributionSizeSequence 6
+```haskell
+[1,6,21,56,126,252,462,792,1287,2002,3003,4368,6188,8568,11628,15504
+```
+[A000389](https://oeis.org/A000389)  Binomial coefficients C(n,5).
+
+### distributionSizeSequence 7
+```haskell
+[1,7,28,84,210,462,924,1716,3003,5005,8008
+```
+[A000579](https://oeis.org/A000579)  Figurate numbers or binomial coefficients C(n,6).
--- library/Distribution.hs
+++ library/Distribution.hs
@@ -4,25 +4,85 @@ import Numeric.Natural (Natural)
 import qualified System.Environment
 import qualified Text.Read
 
-possibleArrays :: Natural -> Natural -> [[Natural]]
-possibleArrays sizeOfElement numberOfElements | numberOfElements == 0 = [[]]
-possibleArrays sizeOfElement numberOfElements =
-  let possibleElements :: [Natural]
-      possibleElements = [0..sizeOfElement]
+-- | For a given number, enumerate the numbers from 1 to that number.
+-- Example: 'possibleValuesStartingAtOne 5 == [1,2,3,4,5]'
+-- Property: 'length (possibleValuesStartingAtOne n) == n'
+possibleValuesStartingAtOne :: Natural -> [Natural]
+possibleValuesStartingAtOne count = [1..count]
 
-      subArrays :: [[Natural]]
-      subArrays = possibleArrays sizeOfElement (numberOfElements - 1)
+-- | For a given number, enumerate the numbers from 0 to that number.
+-- Example: 'possibleValuesStartingAtOneIncludingZero 5 == [0,1,2,3,4,5]'
+-- Property: 'length (possibleValuesStartingAtOneIncludingZero n) == n + 1'
+possibleValuesStartingAtOneIncludingZero :: Natural -> [Natural]
+possibleValuesStartingAtOneIncludingZero count = 0 : possibleValuesStartingAtOne count
+
+possibleArrays :: [Natural] -> Natural -> [[Natural]]
+possibleArrays possibleValues numberOfElements | numberOfElements == 0 = [[]]
+possibleArrays possibleValues numberOfElements =
+  let subArrays :: [[Natural]]
+      subArrays = possibleArrays possibleValues (numberOfElements - 1)
 
       perElement :: Natural -> [[Natural]]
       perElement x = fmap (\xs -> x : xs) subArrays
 
-  in concatMap perElement possibleElements
+  in concatMap perElement possibleValues
 
-distCheck :: Natural -> Natural -> [[Natural]]
-distCheck sizeOfElement numberOfElements =
-  let all = possibleArrays numberOfElements sizeOfElement
-      check xs = sum xs == numberOfElements
+-- | Return all possible distributions with 'sizeOfElement' possible values per element and 'numberOfElements' total values.
+-- Example: 'distCheck 2 5' is all of the possible counts of 0s and 1s for 5 bits.
+--  0 1  ()
+-- [0,5] means 0 zero-bits and 5 one-bits
+-- [1,4] means 1 zero-bits and 5 one-bits
+-- [2,3]
+-- [3,2]
+-- [4,1]
+-- [5,0]
+distributions :: Natural -> Natural -> [[Natural]]
+distributions sizeOfElement numberOfElements =
+  let -- For a given range of possible values [1..n] (here, 'numberOfElements == n')
+      -- the count can be [0..n] since a possible distribution may have zero occurrences of a particular value
+      possibleCountValues :: [Natural]
+      possibleCountValues = possibleValuesStartingAtOneIncludingZero numberOfElements
+
+      -- the distribution is an array where the elements are the count of how many times that value occurs,
+      -- and the value is the 1-based index into the array
+      all :: [[Natural]]
+      all = possibleArrays possibleCountValues sizeOfElement
+
+      -- A valid distribution is where the sum of the counts is equal to the numberOfElements input.
+      check :: [Natural] -> Bool
+      check counts = sum counts == numberOfElements
   in filter check all
+
+-- | An integer sequence of number of possible distributions of the given element cardinality.
+-- Examples:
+
+-- distributionSizeSequence 3
+-- [1,3,6,10,15,21,28,36,45,55,66,78,91,105,120,136,153,171,190,210,231,253,276,300,325,351,378,406,435,465,496,528,561,595,630,666,703,741,780,820,861,903,946,990,1035,1081,1128,1176,1225,1275,1326,1378
+-- A000217  Triangular numbers: a(n) = binomial(n+1,2) = n(n+1)/2 = 0 + 1 + 2 + ... + n.
+-- A161680  a(n) = binomial(n,2): number of size-2 subsets of {0,1,...,n} that contain no consecutive integers.
+-- A105340  a(n) = n*(n+1)/2 mod 2048.
+
+-- distributionSizeSequence 4
+-- [1,4,10,20,35,56,84,120,165,220,286,364,455,560,680,816,969,1140,1330,1540,1771,2024,2300,2600,2925,3276,3654,4060,4495,4960,5456
+-- A000292  Tetrahedral (or triangular pyramidal) numbers: a(n) = C(n+2,3) = n*(n+1)*(n+2)/6.
+
+-- distributionSizeSequence 5
+-- [1,5,15,35,70,126,210,330,495,715,1001,1365,1820,2380,3060,3876,4845,5985,7315,8855
+-- A000332  Binomial coefficient binomial(n,4) = n*(n-1)*(n-2)*(n-3)/24.
+
+-- distributionSizeSequence 6
+-- [1,6,21,56,126,252,462,792,1287,2002,3003,4368,6188,8568,11628,15504
+-- A000389  Binomial coefficients C(n,5).
+
+-- distributionSizeSequence 7
+-- [1,7,28,84,210,462,924,1716,3003,5005,8008
+-- A000579  Figurate numbers or binomial coefficients C(n,6).
+
+distributionSizeSequence :: Natural -> [Natural]
+distributionSizeSequence sizeOfElement =
+  let perLength :: Natural -> Natural
+      perLength len = fromIntegral $ length $ distributions sizeOfElement len
+  in fmap perLength [0..]
 
 main :: IO ()
 main = do
@@ -34,4 +94,6 @@ main = do
         , Just numberOfElements <- Text.Read.readMaybe y
         -> return (sizeOfElement, numberOfElements)
       _ -> fail "Pass two natural numbers as arguments"
-  mapM_ print $ distCheck sizeOfElement numberOfElements
+  putStrLn $ "sizeOfElement: " <> show sizeOfElement
+  putStrLn $ "numberOfElements: " <> show numberOfElements
+  mapM_ print $ distributions sizeOfElement numberOfElements
